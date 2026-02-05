d424da3b0dceef6a71e758a88992789b9abee7b0
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-05 11:42:19 +0100

    fixed commons

--- homework/anastasiia_merkulova/Homework_5/task_2.py
+++ homework/anastasiia_merkulova/Homework_5/task_2.py
@@ -1,20 +1,16 @@
 all_text_one = 'результат операции: 42'
-list_from_all_text_one = all_text_one.split()
-result_one = list_from_all_text_one[-1]
-result_one = int(result_one) + 10
-
+number_index_one = all_text_one.index('42')
+result_one = int(all_text_one[number_index_one:]) + 10
 print(result_one)
 
 all_text_two = 'результат операции: 514'
-list_from_all_text_two = all_text_two.split()
-result_two = list_from_all_text_two[-1]
-result_two = int(result_two) + 10
+number_index_two = all_text_two.index('514')
+result_two = int(all_text_two[number_index_two:]) + 10
 
 print(result_two)
 
 all_text_three = 'результат работы операции: 9'
-list_from_all_text_three = all_text_three.split()
-result_three = list_from_all_text_three[-1]
-result_three = int(result_three) + 10
+number_index_three = all_text_three.index('9')
+result_three = int(all_text_three[number_index_three:]) + 10
 
 print(result_three)
