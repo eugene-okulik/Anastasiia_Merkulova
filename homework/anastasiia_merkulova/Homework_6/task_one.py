211ed9691a2594f3a66560252a8efbb312d974da
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-05 11:19:04 +0100

    save

---
+++ homework/anastasiia_merkulova/Homework_6/task_one.py
@@ -0,0 +1,7 @@
+first_text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel.'
+              'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+)
+
+for word in first_text:
+    print(word)
+
