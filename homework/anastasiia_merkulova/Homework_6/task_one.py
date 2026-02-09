11d7e2f0603741ece5ee8447396d35c078962a0c
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-09 10:26:40 +0100

    completed homework 6

--- homework/anastasiia_merkulova/Homework_6/task_one.py
+++ homework/anastasiia_merkulova/Homework_6/task_one.py
@@ -1,7 +1,12 @@
-first_text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel.'
+first_text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
               'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 )
+first_text = first_text.split( )
 
-for word in first_text:
-    print(word)
+for item in first_text:
+    if item.endswith(',') or item.endswith(".") :
+        item = item[:-1] +'ing'
+    else:
+        item = item + 'ing'
+    print(item)
 
