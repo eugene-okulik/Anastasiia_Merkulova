a6968a4148a1f56cd30a3e347b0aaa560363e48c
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-09 12:19:38 +0100

    corrected issues

--- homework/anastasiia_merkulova/Homework_5/task_2.py
+++ homework/anastasiia_merkulova/Homework_5/task_2.py
@@ -1,16 +1,17 @@
 all_text_one = 'результат операции: 42'
-number_index_one = all_text_one.index('42')
-result_one = int(all_text_one[number_index_one:]) + 10
-print(result_one)
+number_index_one = all_text_one.index(':')
+number_one = int(all_text_one[number_index_one + 1:]) + 10
+
+print(number_one)
 
 all_text_two = 'результат операции: 514'
-number_index_two = all_text_two.index('514')
-result_two = int(all_text_two[number_index_two:]) + 10
+number_index_two = all_text_two.index(':')
+number_two = int(all_text_two[number_index_two + 1:]) + 10
 
-print(result_two)
+print(number_two)
 
 all_text_three = 'результат работы операции: 9'
-number_index_three = all_text_three.index('9')
-result_three = int(all_text_three[number_index_three:]) + 10
+number_index_three = all_text_three.index(':')
+number_three = int(all_text_three[number_index_three + 1:]) + 10
 
-print(result_three)
+print(number_three)
