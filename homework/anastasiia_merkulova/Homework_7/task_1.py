bd2c8aad500271609fa47d032b98be7a2ba9d795
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-11 20:40:59 +0100

    save first task

---
+++ homework/anastasiia_merkulova/Homework_7/task_1.py
@@ -0,0 +1,20 @@
+def right_number(program_number):
+    while True:
+        my_number = int(input('Enter a number:'))
+        if my_number == program_number:
+         print('Поздравляю!Вы угадали!')
+         break
+        elif my_number != program_number:
+         print('Попробуйте снова!')
+        continue
+right_number(100)
+
+
+
+
+
+
+
+
+
+
