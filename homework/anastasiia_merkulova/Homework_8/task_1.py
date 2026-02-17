17b11ad09d01109adb45152d44a78fe1120343ef
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-17 11:14:24 +0100

    saved first task

---
+++ homework/anastasiia_merkulova/Homework_8/task_1.py
@@ -0,0 +1,12 @@
+from random import random, randint, randrange, choice
+
+salary = int(input("Enter your salary: "))
+bonus = choice([True, False])
+if bonus == True:
+    random_bonus = randint(40, 1000) + salary
+    print (f"{salary}, {bonus} - '$'{random_bonus}")
+else:
+    print(f"{salary}, {bonus} - '$'{salary}")
+
+
+
