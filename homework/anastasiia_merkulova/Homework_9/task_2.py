c30603e3b1afd544b45cfc91e39da9b7c615f05c
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-23 11:22:44 +0100

    completed homework_9

---
+++ homework/anastasiia_merkulova/Homework_9/task_2.py
@@ -0,0 +1,12 @@
+temperature = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34,
+               30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33
+]
+high_temperature = []
+for x in temperature:
+    if x > 28:
+        high_temperature.append(x)
+
+print(high_temperature)
+print(max(high_temperature))
+print(min(high_temperature))
+print(sum(high_temperature) / len(high_temperature))
