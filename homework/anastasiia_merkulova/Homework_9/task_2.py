9e2584ded43379a1f683c2354a5a12a3c6047e94
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-25 11:44:06 +0100

    fixed lint issues

--- homework/anastasiia_merkulova/Homework_9/task_2.py
+++ homework/anastasiia_merkulova/Homework_9/task_2.py
@@ -1,6 +1,10 @@
 temperature = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33]
+
+
 def check_temperature(x):
     return x > 28
+
+
 high_temperature = filter(check_temperature, temperature)
 high_temperature = list(high_temperature)
 
