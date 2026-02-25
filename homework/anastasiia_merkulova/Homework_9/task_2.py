19cfccab409d999c9d9208e7143838ac89b933da
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-02-25 11:40:03 +0100

    redid task 2

--- homework/anastasiia_merkulova/Homework_9/task_2.py
+++ homework/anastasiia_merkulova/Homework_9/task_2.py
@@ -1,8 +1,8 @@
 temperature = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33]
-high_temperature = []
-for x in temperature:
-    if x > 28:
-        high_temperature.append(x)
+def check_temperature(x):
+    return x > 28
+high_temperature = filter(check_temperature, temperature)
+high_temperature = list(high_temperature)
 
 print(high_temperature)
 print(max(high_temperature))
