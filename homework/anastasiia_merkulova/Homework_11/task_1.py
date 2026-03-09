f0afbec6de78b1a84017786c27b19c1d3d8a5659
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-03-09 10:52:32 +0100

    fixed lint issues

--- homework/anastasiia_merkulova/Homework_11/task_1.py
+++ homework/anastasiia_merkulova/Homework_11/task_1.py
@@ -25,6 +25,7 @@ class Book:
                   f'Number of pages: {self.number_of_pages}\n'
                   f'Page material: {self.page_material}\n')
 
+
 first_book = Book('The Silent Forest', 'Emma Carter', 320, '978-1-4028-9462-6')
 second_book = Book('Journey to the Red Planet', 'Daniel Brooks', 280, '978-1-545-01022-1')
 third_book = Book('The Art of Simple Living', 'Sophia Bennett', 210, '978-1-86197-876-9')
@@ -33,13 +34,14 @@ fifth_book = Book('Ocean Mysteries', 'laura Williams', 350, '978-1-250-30689-3')
 
 first_book.reserve_book()
 
+
 class SchoolBooks(Book):
 
     def __init__(self, book_title, author, number_of_pages, isbn, subject, grade, include_exercises):
-         super().__init__(book_title, author, number_of_pages, isbn)
-         self.subject = subject
-         self.grade = grade
-         self.include_exercises = include_exercises
+        super().__init__(book_title, author, number_of_pages, isbn)
+        self.subject = subject
+        self.grade = grade
+        self.include_exercises = include_exercises
 
     def get_book_info(self):
         if self.reserved:
@@ -55,6 +57,7 @@ class SchoolBooks(Book):
                   f'Number of pages: {self.number_of_pages}\n'
                   f'Subject: {self.subject}\n'
                   f'Grade: {self.grade}\n')
+
 
 first_schoolbook = SchoolBooks('Mathematics,Grade 10', 'Emma Smith', 280, '918-1-4028-9462-6', 'Mathematics', 10, True)
 second_schoolbook = SchoolBooks('History Grade 9', 'Jim Taylor', 310, '918-1-4028-9002-6', 'History', 12, False)
