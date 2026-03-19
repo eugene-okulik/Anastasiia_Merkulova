40d7efaf36dd058b207192be470888f6d882e720
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-03-19 20:35:39 +0100

    completed task 1

--- homework/anastasiia_merkulova/Homework_13/task_1.py
+++ homework/anastasiia_merkulova/Homework_13/task_1.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+from datetime import datetime, timedelta
 
 base_path = os.path.dirname(__file__)
 file_path = os.path.dirname(os.path.dirname(base_path))
@@ -11,12 +11,20 @@ def read_file():
         for line in data_file.readlines():
             yield line
 
-for data_file in read_file():
-    data_file = data_file.split()
-    data_file = data_file[1] +  ' ' + data_file[2]
-    print(data_file)
 
-strip склеить строки и поменять даты
+for index, data_file in enumerate(read_file()):
+    data_file = data_file.split()
+    data_file = data_file[1] + ' ' + data_file[2]
+    data_file = datetime.strptime(data_file, '%Y-%m-%d %H:%M:%S.%f')
+    now = datetime.now()
+
+    if index == 0:
+        print(data_file + timedelta(days=7))
+    elif index == 1:
+        print(data_file.strftime('%A'))
+    elif index == 2:
+        print((now - data_file).days)
+
 
 
 
