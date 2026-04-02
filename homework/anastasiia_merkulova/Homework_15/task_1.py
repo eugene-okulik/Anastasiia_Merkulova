cb877726e189b975d59d3a546496303f1dc4c050
Anastasiia Merkulova <anastasiia14merk@gmail.com>
2026-04-02 21:03:50 +0200

    completed lint issues

--- homework/anastasiia_merkulova/Homework_15/task_1.py
+++ homework/anastasiia_merkulova/Homework_15/task_1.py
@@ -13,7 +13,8 @@ second_name = input('Enter your second name: ')
 
 
 cursor = db.cursor(dictionary=True)
-cursor.execute("INSERT INTO students(name, second_name) VALUES (%s, %s)",
+cursor.execute(
+    "INSERT INTO students(name, second_name) VALUES (%s, %s)",
     (name, second_name)
 )
 student_id = cursor.lastrowid
@@ -22,7 +23,8 @@ print(f"student_id: {student_id}, name: {name}, second_name: {second_name}")
 title = input('Enter group title: ')
 start_date = input('Enter start date: ')
 end_date = input('Enter end date: ')
-cursor.execute("INSERT INTO `groups`(title, start_date, end_date) VALUES (%s, %s, %s)",
+cursor.execute(
+    "INSERT INTO `groups`(title, start_date, end_date) VALUES (%s, %s, %s)",
     (title, start_date, end_date)
 )
 group_id = cursor.lastrowid
@@ -32,13 +34,15 @@ books_count = int(input('Enter a number of books: '))
 taken_by_student_id = student_id
 for book in range(books_count):
     book_title = input('Enter book title: ')
-    cursor.execute("INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)",
-        (book_title, taken_by_student_id)
+    cursor.execute(
+    "INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)",
+    (book_title, taken_by_student_id)
     )
     book_id = cursor.lastrowid
     print(f"book_id: {book_id}, title: {book_title}, taken_by_student_id: {taken_by_student_id}")
 
-cursor.execute("UPDATE students SET group_id = %s WHERE id = %s",
+cursor.execute(
+    "UPDATE students SET group_id = %s WHERE id = %s",
     (group_id, student_id)
 )
 
@@ -46,20 +50,23 @@ cursor.execute("UPDATE students SET group_id = %s WHERE id = %s",
 subjects_count = int(input('Enter a number of subjects: '))
 for subject in range(subjects_count):
     subject_title = input('Enter subject title: ')
-    cursor.execute("INSERT INTO subjects (title) VALUES (%s)",
+    cursor.execute(
+        "INSERT INTO subjects (title) VALUES (%s)",
         (subject_title,)
     )
     subject_id = cursor.lastrowid
 
     for lesson in range(2):
         lesson_title = input(f"Enter lesson title {subject_title}: ")
-        cursor.execute("INSERT INTO lessons(title, subject_id) VALUES (%s, %s)",
+        cursor.execute(
+            "INSERT INTO lessons(title, subject_id) VALUES (%s, %s)",
             (lesson_title, subject_id)
         )
         lesson_id = cursor.lastrowid
 
         value = int(input(f"Enter mark for {lesson_title}:  "))
-        cursor.execute("INSERT INTO marks(value, lesson_id, student_id) VALUES (%s, %s, %s)",
+        cursor.execute(
+            "INSERT INTO marks(value, lesson_id, student_id) VALUES (%s, %s, %s)",
             (value, lesson_id, student_id)
         )
         marks_id = cursor.lastrowid
